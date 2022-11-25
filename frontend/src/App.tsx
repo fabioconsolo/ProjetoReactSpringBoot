6bb5532e2ab3c331b5d51c03bba0e043993dd3f8
Fabio Matarazzo <fabio.consolo@yahoo.com.br>
2022-11-25 13:04:49 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import NotificationButton from "./componentes/BotaoNotificacao"
 import Header from "./componentes/Header"
 import SalesCard from "./componentes/SalesCard"
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
     return (
         <>
+         <ToastContainer/>
             <Header />
             <main>
                 <section id="sales">
